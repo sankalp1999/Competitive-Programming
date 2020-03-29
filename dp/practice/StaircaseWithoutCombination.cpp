4fc7dc54a425cf955c7a7ee571d3c880b75d773f
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-03-29 19:00:19 +0530

    Create StaircaseWithoutCombination.cpp

---
+++ dp/practice/StaircaseWithoutCombination.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	int p = 1e6 + 7;
+	int dp[p]; // array 
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 2;
+	for(int i = 4; i <= p; i++)
+	{
+	    if(i%2==0)
+	        dp[i] = dp[i-1]+1;
+	   else
+            dp[i] = dp[i-1];
+	}
+	while(t--)
+	{
+	    int k;
+	    cin >> k;
+	    cout << dp[k] << endl;
+	}
+	return 0;
+}
