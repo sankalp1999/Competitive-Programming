00479f2042f22118faac5ea94d05e2eab50fc3e6
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-05-31 19:17:33 +0530

    Update SearchA2DMatrix2_leetcode.cpp

--- DivideConquer/SearchA2DMatrix2_leetcode.cpp
+++ DivideConquer/SearchA2DMatrix2_leetcode.cpp
@@ -2,6 +2,13 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& arr, int target) {
         
+        
+        // The idea is to search from the top right corner or the bottom left corner
+        // These offer two choices in two directions
+        // suppose, target value is greater, then only going down will increase value.
+        // If target value is smaller, then only going towards left will decrease value.
+        // O(m+n) solution.
+        
         int m = arr.size();
         if(m==0)return false;
         int n = arr[0].size();
