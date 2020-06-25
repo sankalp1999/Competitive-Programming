a9942d88f126d646ed94010d11987a57d3ba1e42
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-06-25 16:06:22 +0530

    Added the good explanation found on Leetcode

--- InterviewBit/TwoPointers/Container_with_most_water.cpp
+++ InterviewBit/TwoPointers/Container_with_most_water.cpp
@@ -11,7 +11,14 @@ This is similar to Three Pointers question in InterviewBit Two pointers question
 
 */
 
-
+/* LEETCODE EXPLANATION
+Initially we consider the area constituting the exterior most lines. Now, to maximize the area, 
+we need to consider the area between the lines of larger lengths. If we try to move the pointer at the 
+longer line inwards, we won't gain any increase in area, since it is limited by the shorter line. But
+moving the shorter line's pointer could turn out to be beneficial, as per the same argument, 
+despite the reduction in the width. This is done since a relatively 
+longer line obtained by moving the shorter line's pointer might overcome the reduction in area caused by the width reduction.
+*/
 
 
 int Solution::maxArea(vector<int> &height) {
