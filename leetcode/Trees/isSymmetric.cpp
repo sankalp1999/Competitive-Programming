ff61ac3dda5c3ee669e21e4df43b216eb4323ce7
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-06-27 11:26:28 +0530

    Update isSymmetric.cpp

--- leetcode/Trees/isSymmetric.cpp
+++ leetcode/Trees/isSymmetric.cpp
@@ -1,14 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+// This is like seeing in a mirror. Your head is at the same position
+// but your hands are inversed!!!!!!
+// Two policies required here
+// a) leftSub.left = rightSub.right
+// b) leftSub.right = rightSub.left
+
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
