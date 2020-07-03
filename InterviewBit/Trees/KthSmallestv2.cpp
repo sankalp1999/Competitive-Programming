fd8ce81c70f3667646bc9d54d39aab69ad5d0827
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-03 23:51:04 +0530

    Update KthSmallestv2.cpp

--- InterviewBit/Trees/KthSmallestv2.cpp
+++ InterviewBit/Trees/KthSmallestv2.cpp
@@ -6,6 +6,25 @@ we print a value.
 So, when k becomes zero, we have reached the kth smallest element.
 */
 
+/* Recursive Editorial Solution First */
+class Solution {
+    public:
+        int find(TreeNode* root, int &k) {
+            if (!root) return -1;
+            // We do an inorder traversal here. 
+            int k1 = find(root->left, k);
+            if (k == 0) return k1; // left subtree has k or more elements.
+            k--; 
+            if (k == 0) return root->val; // root is the kth element.
+            return find(root->right, k); // answer lies in the right node.
+        }
+
+        int kthsmallest(TreeNode* root, int k) {
+           return find(root, k); // Call another function to pass k by reference.
+        }
+};
+
+/* My solution is iterative */
 
 int Solution::kthsmallest(TreeNode* root, int k) {
     
