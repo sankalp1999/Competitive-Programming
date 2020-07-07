52717576ca4a58ea2b9138987b71b6266be25691
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-07 13:53:07 +0530

    Update Lru_Cache.cpp

--- InterviewPrep/InterviewBit/Heaps_Maps/Lru_Cache.cpp
+++ InterviewPrep/InterviewBit/Heaps_Maps/Lru_Cache.cpp
@@ -1,6 +1,25 @@
 #include <list>
 #include <unordered_map>
 
+/* 
+LeetCode implementation uses better variables. So, check out that.
+
+Main idea of the problem
+1. HashTable helps in giving O(1) lookups.
+2. Doubly Linked List gives O(1) removals because only links have to be changed. To directly reach the node,
+   store them in the hash table along with the value.
+Important details
+1. If a key is already there and it is again being inserted with a new value, you have to replace that key with the new value.
+   In order to do this, one is supposed to delete the already existing key value pair and then insert the new one.
+   The capacity of the hashtable will remain the same in this operation.
+2. If we lookup a value, then we find it's current position in the DLL and remove it. We add that key to 
+   to the front because its now the most recently used value. 
+   So, we need to re-insert it into hashtable with new iterator address and of course, we need to remove 
+   previous occurence from the DLL and lookup table and create a new one.
+3. If capacity of hashtable is full, it's time to simply delete the last key in the DLL. 
+4. If you check after pushing, then c > capacity otherwise c == capacity 
+*/
+
 unordered_map<int, pair<list<int>::iterator,int>> m;
 list<int> l;
 int c;
