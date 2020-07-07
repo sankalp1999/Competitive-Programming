d09864387bacef4bf3ff9c6af380e5c24cb05fc2
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-07 18:19:11 +0530

    Update MaximumSumCombination.cpp

--- InterviewPrep/InterviewBit/Heaps_Maps/MaximumSumCombination.cpp
+++ InterviewPrep/InterviewBit/Heaps_Maps/MaximumSumCombination.cpp
@@ -1,3 +1,22 @@
+
+/*
+Explanation at leetcode for smallest variation of this question
+https://leetcode.com/problems/find-k-pairs-with-smallest-sums/discuss/84607/Clean-16ms-C%2B%2B-O(N)-Space-O(KlogN)-Time-Solution-using-Priority-queue
+
+Sure. Suppose you are at pair 0,0 (index 0 and index 0, not value), which is the current minimum.
+Then you know the only two possible followers (immediate larger ones) are 0,1 and 1,0. Any other indices, say 1,1, 1,2, 3,3 have to be larger right?
+So every time you get a current minimum i,j , you want to push i+1,j and i,j+1 into heap. You don't need to worry about others yet.
+
+The problem here is, if you are at 2,3, you will push 3,3 and 2,4; then later, you are at 3,2. Then you will push 3,3, 4,2. so you pushed 3,3 twice.
+
+But it is easy to realize, if you are at 2,3, and you haven't seen 3,2 yet (meaning 3,2 is either still in the queue but not popped yet or not even pushed into queue), you don't need to worry about 3,3 at this moment, because 3,2 is guaranteed to be no greater than 3,3. So you can wait till you see 3,2.
+
+This basically means every time you see i,j you just need to push i+1, j into queue. i, j+1 can be pushed into queue later when you see i - 1, j + 1. The only exception to this is, when i == 0, there is no i-1, j+1 anymore, so you want to push both i+1, j and i, j+1 when i == 0.
+*/
+
+
+
+
 /* 
 Better than brute force solution.
 O(N log N ) solution. (Because of sorting but otherwise O(N log k) solution.
