7d3f5f126b7c324f56b74c45d67bfc537749678a
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-08 13:23:56 +0530

    Create SortAKSortedArray.cpp

---
+++ GFG/SortAKSortedArray.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+/*
+The elements are max to max k away to the right in the array.
+So, we need to maintain a heap of size k. 
+That way, if we keep touching size k heap,
+then we will always have the min element at the top.
+
+K log N solution.
+
+*/
+
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        priority_queue<int,vector<int>, greater<int>> pq;
+        int i;
+        for(i = 0;i < k; i++)
+        {
+            pq.push(v[i]);
+        }
+        
+        while(i < n && !pq.empty())
+        {
+            auto idx = pq.top();
+            pq.pop();
+            cout << idx << " ";
+            pq.push(v[i++]);
+        }
+        while(!pq.empty())
+        {
+            auto idx = pq.top();
+            pq.pop();
+            cout << idx << " ";
+        }
+        cout << endl;
+    }
+	return 0;
+}
