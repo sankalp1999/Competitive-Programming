8d93dd05a6af66f96766a0c9dfc768b9931317d9
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-08 10:56:10 +0530

    Update Constructing_Tree_From_PreOrder_Inorder.cpp

--- InterviewPrep/InterviewBit/Trees/Constructing_Tree_From_PreOrder_Inorder.cpp
+++ InterviewPrep/InterviewBit/Trees/Constructing_Tree_From_PreOrder_Inorder.cpp
@@ -1,12 +1,15 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
+/* Explanation for right prestart
+I hope this helps those folks who can't figure out how to get the index of the right child:
+
+Our aim is to find out the index of right child for current node in the preorder array
+We know the index of current node in the preorder array - preStart (or whatever your call it), it's the root of a subtree
+Remember pre order traversal always visit all the node on left branch before going to the right ( root -> left -> ... -> right), therefore, we can get the immediate right child index by skipping all the node on the left branches/subtrees of current node
+The inorder array has this information exactly. Remember when we found the root in "inorder" array we immediately know how many nodes are on the left subtree and how many are on the right subtree
+Therefore the immediate right child index is preStart + numsOnLeft + 1 (remember in preorder traversal array root is always ahead of children nodes but you don't know which one is the left child which one is the right, and this is why we need inorder array)
+numsOnLeft = root - inStart.
+*/
+
 TreeNode* helper(vector<int>& pre, vector<int>& in, int is, int ie, int ps, int pe, unordered_map<int,int>& m)
 {
     if(is > ie)return NULL;
@@ -36,7 +39,7 @@ TreeNode* Solution::buildTree(vector<int> &pre, vector<int> &in) {
     int is, ie, ps, pe;
     is = 0;
     ps = 0;
-    ie = n -1;
-    pe = n -1 ;
+    ie = n - 1;
+    pe = n - 1;
     return helper(pre, in, is, ie, ps, pe, m);
 }
