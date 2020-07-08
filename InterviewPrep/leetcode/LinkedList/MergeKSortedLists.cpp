7884985f2ef2cb54db96885a6ba4363f384828f5
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-08 11:55:53 +0530

    Added explanation

--- InterviewPrep/leetcode/LinkedList/MergeKSortedLists.cpp
+++ InterviewPrep/leetcode/LinkedList/MergeKSortedLists.cpp
@@ -18,6 +18,13 @@ public:
         // Since curr took start's reference,
         // curr is manipulating the start list
         priority_queue<pli,vector<pli>,greater<pli>> pq;
+        
+        // Use heap to keep track of minimum element
+        // Keep pair<int,ListNode*>
+        // So as to keep track of next node
+        // and heap is ordered by the first element of 
+        // the priority queue, so keep it first.
+        
         for(int i = 0 ;i < lists.size(); i++)
         {
             if(lists[i])
@@ -25,6 +32,10 @@ public:
                 pq.push({lists[i]->val,lists[i]});
             }
         }
+        // Don't let the size of the heap grow more than k
+        // That way we will maintain the minimum of the k lists one entry.
+        // Since we need to look at atleast k entries to decide 
+        // the next node.
         while(!pq.empty())
         {
            {
