39caad1149cc09afb3dfd0b550ccdbf7ad44822b
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-09 17:39:53 +0530

    Create LongestSubstringWithoutRepeat_Editorial_solution.java

---
+++ InterviewPrep/leetcode/Hashing/LongestSubstringWithoutRepeat_Editorial_solution.java
@@ -0,0 +1,22 @@
+
+/* This solution is slightly better than my solution. It has been taken from the Leetcode editorial */
+
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
