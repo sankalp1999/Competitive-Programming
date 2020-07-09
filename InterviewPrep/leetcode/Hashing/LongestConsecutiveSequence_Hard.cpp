91de84d11be762a0d1d3666735b7b5709374105c
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-09 15:28:41 +0530

    Update LongestConsecutiveSequence_Hard.cpp

--- InterviewPrep/leetcode/Hashing/LongestConsecutiveSequence_Hard.cpp
+++ InterviewPrep/leetcode/Hashing/LongestConsecutiveSequence_Hard.cpp
@@ -3,6 +3,11 @@
 The main idea is that if check for both 1 greater and 1 lesser element,
 then we are double contributing. ( I was doing this earlier)
 
+Golden points
+We only attempt to build sequences from numbers that are not already part of a longer sequence.
+This is accomplished by first ensuring that the number that would immediately precede the current number in a 
+sequence is not present, as that number would necessarily be part of a longer sequence.
+
 1. First, we remove all the duplicates from the array. We do this using the unordered_set.
 2. Now, as we traverse, [100,200,1,3,2,4] suppose, we come across 1, then we search for 2, if we find 2, then we search for 3 and then we 
    search for 4. Search is O(1) since we are using unordered_set.
