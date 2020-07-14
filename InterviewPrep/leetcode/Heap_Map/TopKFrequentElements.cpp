580cd18783a92cf2bb1ce34dc4b7b1cddbbb3f28
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-15 00:23:35 +0530

    Create TopKFrequentElements.cpp

---
+++ InterviewPrep/leetcode/Heap_Map/TopKFrequentElements.cpp
@@ -0,0 +1,37 @@
+/*
+Count the frequency using a hashmap and then store all 
+the elements in the maxheap. Since, we want the 
+ones with the higher frequency first, we use a max heap.
+Make sure that the frequency is the first quantity 
+in the pair.
+*/
+class Solution {
+public:
+    typedef pair<int,int> pii;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pii> pq;
+        unordered_map<int,int> freq;
+        
+        for(int i = 0;i < nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size(); i++)
+        {
+            auto it = freq.find(nums[i]);
+            if(it != freq.end())
+            {
+                 pq.push({it->second, it->first});
+                 freq.erase(it->first);
+            }
+        }
+        vector<int> v;
+        while(k--)
+        {
+            auto idx = pq.top();
+            pq.pop();
+            v.push_back(idx.second);
+        }
+        return v;
+        
+    }
+};
