532d026912470d882bb5cf5629896c6b30decfeb
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-19 14:39:30 +0530

    Create FloodFill.py

---
+++ InterviewPrep/leetcode/Graph/FloodFill.py
@@ -0,0 +1,18 @@
+
+/* A great Python solution I found on LeetCode by YangShun.
+https://leetcode.com/problems/flood-fill/discuss/109604/Easy-Python-DFS-(no-need-for-visited)!!!
+*/
+
+
+
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        rows, cols, orig_color = len(image), len(image[0]), image[sr][sc]
+        def traverse(row, col):
+            if (not (0 <= row < rows and 0 <= col < cols)) or image[row][col] != orig_color:
+                return
+            image[row][col] = newColor
+            [traverse(row + x, col + y) for (x, y) in ((0, 1), (1, 0), (0, -1), (-1, 0))]
+        if orig_color != newColor:
+            traverse(sr, sc)
+        return image
