80f3ffd0764fcd1e47dc4bb63bddceb224b1c987
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-21 20:59:32 +0530

    Update HammingDistance.cpp

--- InterviewPrep/leetcode/Bit_Manipulation/HammingDistance.cpp
+++ InterviewPrep/leetcode/Bit_Manipulation/HammingDistance.cpp
@@ -4,3 +4,15 @@ public:
         return __builtin_popcount(x ^ y);
     }
 };
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int dist = 0, n = x ^ y;
+        while (n) {
+            ++dist;
+            n &= n - 1;
+        }
+        return dist;
+    }
+};
