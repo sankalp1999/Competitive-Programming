c9523543e91fee28c5d57a2eddf8f31e523454c5
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-21 23:55:26 +0530

    Update GeneratePowerset.cpp

--- InterviewPrep/leetcode/Recursion_Backtracking/GeneratePowerset.cpp
+++ InterviewPrep/leetcode/Recursion_Backtracking/GeneratePowerset.cpp
@@ -6,11 +6,43 @@ Then, we do consider a particular element.
 
 Choose or not choose
 
-Time complexity : O( 2 ^ n ) solution.
+Recurrence relation C(n) = 2 * C(n-1) 
+Time and space complexity : O( 2 ^ n ) solution.
 
 */
+class Solution {
+public:
+    vector<vector<int>> p;
+    int numsize;
+        void subset ( vector<int> v , int n, vector<int> arr)
+    {
+        if(n == numsize) // base case
+        {
+            p.push_back(v);
+            return;
+        }
+        v.push_back(arr[n]); // select the element and generate subset
+        subset(v, n+1, arr);
+        v.pop_back();  // don't select the element and generate subset
+        subset(v, n + 1, arr);
+    }
+    
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<int> v;
+        numsize = nums.size();
+        subset(v, 0, nums);
+        return p;
+        
+    }
+};
 
 
+/* Solution 2
+Based on a similar logic
+*/
 
 class Solution {
 public:
