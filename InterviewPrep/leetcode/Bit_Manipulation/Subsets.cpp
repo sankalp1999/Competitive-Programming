47ed37e64ad4fe62d7336b8978e48b4e8d779e62
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-22 00:17:54 +0530

    Added more elegant EPI solution.

--- InterviewPrep/leetcode/Bit_Manipulation/Subsets.cpp
+++ InterviewPrep/leetcode/Bit_Manipulation/Subsets.cpp
@@ -15,13 +15,18 @@ only the bits that are set. This way, we can generate the power set.
 To get the bit, (i & ( 1 << j ) ) > 0 ? true : false
 */
 
+/*
+This approach is O( n * 2^n).
+Space complexity O(n)
+
+*/
+
 class Solution {
 public:
     bool getBit(int i, int j)
     {
     return  (i & (1 << j)) > 0 ? true : false;
     }
-    
     
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> v;
@@ -40,3 +45,25 @@ public:
         return v;
     }
 };
+
+/*More elegant solution by EPI book*/
+
+class Solution {
+public:
+  vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> power_set;
+        for(int int_for_subset = 0; int_for_subset < ( 1 << nums.size()); ++int_for_subset)
+        {
+            int bit_array = int_for_subset;
+            vector<int> subset;
+            while(bit_array)
+            {
+    subset.emplace_back( nums[log2( bit_array & ~(bit_array - 1)) ] )  ; // Retrieve the rightmost set bit.
+                bit_array &= bit_array - 1; // Turn off the rightmost set bit.
+            }
+            power_set.emplace_back(subset);  
+        }
+    return power_set;
+ }
+};
+
