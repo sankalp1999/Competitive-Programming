be9e58f699cadf2c8c2e5cab3d4456c0adc50b72
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-22 13:09:21 +0530

    Update CombinationSum2.cpp

--- InterviewPrep/leetcode/Recursion_Backtracking/CombinationSum2.cpp
+++ InterviewPrep/leetcode/Recursion_Backtracking/CombinationSum2.cpp
@@ -11,7 +11,14 @@ public:
         }
     
         for(int i = pos; i < candidates.size(); i++)
-        {
+        {    
+            
+            //i > pos means that we have processed i which is greater than pos.
+            // Now, if current i is equal to previous state's i, then we need to avoid current state as it will lead to 
+            // duplication. Hence we continue
+            // to move into the next state and skip this state.
+            
+            
             if(i>pos && candidates[i] == candidates[i-1])continue; // If next number is same, we don't want to repeat when we backtrack. Cut off that branch.
             p.push_back(candidates[i]);
             dfs(i+1,sum+candidates[i],candidates,v,p, target); // time to move to next state
