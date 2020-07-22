1820dfc7ad577a7b8cb7097c33d6ff520aed8bf1
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-22 13:06:24 +0530

    Create CombinationSum2.cpp

---
+++ InterviewPrep/leetcode/Recursion_Backtracking/CombinationSum2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    void dfs(int pos, int sum, vector<int>& candidates, vector<vector<int>>& v, vector<int>& p, int target)
+    {
+        if(sum > target)return;
+        if(sum == target)
+        {
+            v.push_back(p);
+            return;
+        }
+    
+        for(int i = pos; i < candidates.size(); i++)
+        {
+            if(i>pos && candidates[i] == candidates[i-1])continue; // If next number is same, we don't want to repeat when we backtrack. Cut off that branch.
+            p.push_back(candidates[i]);
+            dfs(i+1,sum+candidates[i],candidates,v,p, target); // time to move to next state
+            p.pop_back(); // backtrack to previous state
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> v;
+        vector<int> p;
+        sort(candidates.begin(), candidates.end());
+        int pos = 0;
+        int sum = 0;
+        dfs(0, sum, candidates, v, p, target);
+        return v;
+    }
+};
