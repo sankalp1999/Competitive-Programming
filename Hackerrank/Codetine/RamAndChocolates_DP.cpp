498e4cbad4f059176a4d6f2da2a684884da06185
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-27 22:43:02 +0530

    Added comments

--- Hackerrank/Codetine/RamAndChocolates_DP.cpp
+++ Hackerrank/Codetine/RamAndChocolates_DP.cpp
@@ -6,13 +6,11 @@
 using namespace std;
 
 /*
-This is essentially similar to the house robber problem
+This is a classic DP problem (House Robber)
 We need to consider the last two entries.
 It can be generalised to k
 We can do that using a for loop
 */
-
-
 
 int main() {
     int t ;
@@ -32,21 +30,25 @@ int main() {
 
         if(n == 1)
         {
-            cout << v[0] << endl;
+            cout << v[0] << endl; // just print it. Nothing to look before that.
             continue;
         }
             else if(n == 2)
         {
-            cout << max(v[0], v[1]) << endl;
+            cout << max(v[0], v[1]) << endl; 
             continue;
         }
        long long int dp[n+1]; 
   
 
+      // Initialize dp array with the best possible entries.
       dp[0] = v[0]; 
-      dp[1] = max(v[0], v[1]); 
+      dp[1] = max(v[0], v[1]);  
   
     // Fill remaining positions 
+        
+     // At each step, we decide either to continue with what we already have and not select current i.e we choose dp[i-1]
+     // or we take current plus the best we could get from before.
      for (int i = 2; i<n; i++) 
         dp[i] = max( (long long )v[i]+ (long long )dp[i-2], (long long )dp[i-1]); 
         
