29f21a711b6708071966a3dd42803de398122494
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-08-18 17:57:23 +0530

    Update ModifySequence.cpp

--- HackerEarth/Standard problems/Array/ModifySequence.cpp
+++ HackerEarth/Standard problems/Array/ModifySequence.cpp
@@ -1,5 +1,8 @@
 /*
-Suppose we have a sequence of non-negative integers, Namely a_1, a_2, ... ,a_n. At each time we can choose one term a_i with 0 < i < n and we subtract 1 from both a_i and a_i+1. We wonder whether we can get a sequence of all zeros after several operations.
+Suppose we have a sequence of non-negative integers, Namely a_1, a_2, ... ,a_n. At each time we can 
+choose one term a_i with 0 < i < n and we subtract 1 from both a_i and a_i+1. 
+We wonder whether we can 
+get a sequence of all zeros after several operations.
 Input
 The first line of test case is a number N. (0 < N <= 10000) The next line is N non-negative integers, 0 <= a_i <= 109
 */
